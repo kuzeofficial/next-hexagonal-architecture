73e2de2da40a98dbeff1d6f79240772f037c2e2c
Cristian Fonseca <cfonsecacomas@gmail.com>
2023-04-01 18:33:19 -0300

    🎨 style(WelcomePeopleCreate.tsx): wrap name and imageUrl inputs in divs 🎨 style(WelcomePeopleCreate.tsx): adjust button height and margin top

--- src/sections/welcome/components/WelcomePeopleCreate/WelcomePeopleCreate.tsx
+++ src/sections/welcome/components/WelcomePeopleCreate/WelcomePeopleCreate.tsx
@@ -20,25 +20,28 @@ const WelcomePeopleCreate = () => {
       onSubmit={handleSubmit(onSubmit)}
       className="mt-8 gap-2 justify-center flex sm:flex-row flex-col"
     >
-      <input
-        className="ring-2 focus:border-none appearance-none bg-transparent  leading-tight focus:outline-none text-white border-blue-300 px-2 py-1 rounded-md"
-        {...register("name")}
-        placeholder="Cristian Fonseca"
-      />
-      <span role="alert" className="text-red-400 mt-1">
-        {errors?.name?.message as string}
-      </span>
-      <input
-        className="ring-2 focus:border-none appearance-none bg-transparent  leading-tight focus:outline-none text-white border-blue-300 px-2 py-1 rounded-md"
-        {...register("imageUrl")}
-        placeholder="Image URL"
-      />
-      <span role="alert" className="text-red-400 mt-1">
-        {errors?.imageUrl?.message as string}
-      </span>
-
+      <div className="flex-col flex py-1">
+        <input
+          className="ring-2 focus:border-none appearance-none bg-transparent  leading-tight focus:outline-none text-white border-blue-300 px-2 py-1 rounded-md"
+          {...register("name")}
+          placeholder="Cristian Fonseca"
+        />
+        <span role="alert" className="text-red-400">
+          {errors?.name?.message as string}
+        </span>
+      </div>
+      <div className="flex-col flex py-1">
+        <input
+          className="ring-2 focus:border-none appearance-none bg-transparent  leading-tight focus:outline-none text-white border-blue-300 px-2 py-1 rounded-md"
+          {...register("imageUrl")}
+          placeholder="Image URL"
+        />
+        <span role="alert" className="text-red-400">
+          {errors?.imageUrl?.message as string}
+        </span>
+      </div>
       <button
-        className="px-4 hover:bg-slate-300 duration-200 transition-all bg-slate-200 rounded-md border-2"
+        className="px-4 max-h-[32px] mt-0.5 hover:bg-slate-300 duration-200 transition-all bg-slate-200 rounded-md "
         type="submit"
       >
         Save
