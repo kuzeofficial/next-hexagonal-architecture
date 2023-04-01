d666d8de0633b76d03acba08d684453715782b7b
Cristian Fonseca <cfonsecacomas@gmail.com>
2023-04-01 17:06:14 -0300

    🚀 feat(page.tsx): add WelcomePeopleCreate and WelcomePeopleList components to Home page 🔧 chore(page.tsx): use WelcomeContextProvider to provide repository to WelcomePeopleCreate and WelcomePeopleList components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+"use client";
+import { WelcomePeopleCreate } from "@/sections/welcome/components/WelcomePeopleCreate";
+import { WelcomePeopleList } from "@/sections/welcome/components/WelcomePeopleList";
+import { WelcomeContextProvider } from "@/sections/welcome/context";
+import { createLocalStoragePeopleRepository } from "@/modules/welcome/infrastructure/LocalStoragePeopleRepository";
+
 export default function Home() {
+  const repository = createLocalStoragePeopleRepository();
+
   return (
-    <main>
+    <WelcomeContextProvider repository={repository}>
       <h1>✨ NextJS Hexagonal Architecture Template🔯</h1>
       <p>Thanks for using this template 🖤</p>
-    </main>
+      <WelcomePeopleList />
+      <WelcomePeopleCreate />
+    </WelcomeContextProvider>
   );
 }
