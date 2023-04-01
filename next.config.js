be031a4a59c54a7cb8b0488b7c466154923df549
Cristian Fonseca <cfonsecacomas@gmail.com>
2023-04-01 17:05:25 -0300

    🚀 chore(next.config.js): add remotePatterns to images configuration 📦 chore(package.json): add new dependencies and devDependencies

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
